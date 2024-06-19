294f0c2689a6952b54756df371d86ffc150953ec
Jonathan Darlan <jdarlan@ufersa.edu.br>
2024-06-19 14:39:41 -0300

    correcao overflow do modal

--- src/components/EntityBrowser/Modal.tsx
+++ src/components/EntityBrowser/Modal.tsx
@@ -11,13 +11,13 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 w-screen h-screen">
+    <div className="fixed inset-0 flex justify-center items-center z-50 w-screen h-screen">
       <div
         className="fixed inset-0 bg-black opacity-50"
         onClick={onClose}
       ></div>
       <div
-        className={`relative ${colors.background.light} dark:${colors.background.dark} rounded shadow-lg z-10 w-[95vw] max-h-[95vh] overflow-y-auto"`}
+        className={`relative ${colors.background.light} dark:${colors.background.dark} rounded shadow-lg z-10 w-[95vw] max-h-[90vh] overflow-y-auto`}
       >
         <button className="absolute top-0 right-0 p-2" onClick={onClose}>
           X
